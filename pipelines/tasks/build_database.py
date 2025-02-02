1a5fc78bfad921b4a31d3cab8978b3f9a116a11b
moreaupascal56 <moreaupascal56@gmail.com>
2025-02-02 22:16:27 +0100

    add fct get_yearly_dataset_infos

--- pipelines/tasks/build_database.py
+++ pipelines/tasks/build_database.py
@@ -4,6 +4,7 @@ Consolidate data into the database.
 
 import logging
 import os
+from typing import Dict
 from zipfile import ZipFile
 
 import duckdb
@@ -12,6 +13,30 @@ import requests
 from ._common import CACHE_FOLDER, DUCKDB_FILE, clear_cache
 
 logger = logging.getLogger(__name__)
+
+def get_yearly_dataset_infos(year:str) -> Dict[str,str]:
+    """
+    Returns information for yearly dataset extract of the SISE Eaux datasets.
+    The data comes from https://www.data.gouv.fr/fr/datasets/resultats-du-controle-sanitaire-de-leau-distribuee-commune-par-commune/
+    For each year a dataset is downloadable on a URL like this (ex. 2024):
+        https://www.data.gouv.fr/fr/datasets/r/84a67a3b-08a7-4001-98e6-231c74a98139
+    The id of the dataset is the last part of this URL
+    The name of the dataset is dis-YEAR.zip (but the format could potentially change).
+    :param year: The year from which we want to get the dataset information.
+    :return: A dict with the id and name of the dataset.
+    """
+    dis_files_info_by_year = {
+        "2024": {"id": "84a67a3b-08a7-4001-98e6-231c74a98139", "name" : "dis-2024.zip"},
+        "2023": {"id":"c89dec4a-d985-447c-a102-75ba814c398e", "name" : "dis-2023.zip"},
+        "2022": {"id":"a97b6074-c4dd-4ef2-8922-b0cf04dbff9a", "name" : "dis-2022.zip"},
+        "2021": {"id":"d2b432cc-3761-44d3-8e66-48bc15300bb5", "name" : "dis-2021.zip"},
+        "2020": {"id":"a6cb4fea-ef8c-47a5-acb3-14e49ccad801", "name" : "dis-2020.zip"},
+        "2019": {"id":"861f2a7d-024c-4bf0-968b-9e3069d9de07", "name" : "dis-2019.zip"},
+        "2018": {"id":"0513b3c0-dc18-468d-a969-b3508f079792", "name" : "dis-2018.zip"},
+        "2017": {"id":"5785427b-3167-49fa-a581-aef835f0fb04", "name" : "dis-2017.zip"},
+        "2016": {"id":"483c84dd-7912-483b-b96f-4fa5e1d8651f", "name" : "dis-2016.zip"}
+    }
+    return dis_files_info_by_year[year]
 
 
 def process_sise_eaux_dataset_2024():
