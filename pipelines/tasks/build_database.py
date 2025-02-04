5358514719cd658d98920a5c392f0013f50860dc
moreaupascal56 <moreaupascal56@gmail.com>
2025-02-04 10:43:35 +0100

    add _config_edc.py and use it accordingly

--- pipelines/tasks/build_database.py
+++ pipelines/tasks/build_database.py
@@ -4,15 +4,17 @@ Consolidate data into the database.
 
 import logging
 import os
-from typing import Dict, List, Literal
+from typing import List, Literal
 from zipfile import ZipFile
 
 import duckdb
 import requests
 
 from ._common import CACHE_FOLDER, DUCKDB_FILE, clear_cache
+from ._config_edc import get_edc_config, create_edc_yearly_filename
 
 logger = logging.getLogger(__name__)
+edc_config = get_edc_config()
 
 
 def check_table_existence(conn: duckdb.DuckDBPyConnection, table_name: str) -> bool:
@@ -31,31 +33,6 @@ def check_table_existence(conn: duckdb.DuckDBPyConnection, table_name: str) -> b
     return list(conn.fetchone())[0] == 1
 
 
-def get_yearly_edc_infos(year: str) -> Dict[str, str]:
-    """
-    Returns information for yearly dataset extract of the EDC (Eau distribuée par commune) datasets.
-    The data comes from https://www.data.gouv.fr/fr/datasets/resultats-du-controle-sanitaire-de-leau-distribuee-commune-par-commune/
-    For each year a dataset is downloadable on a URL like this (ex. 2024):
-        https://www.data.gouv.fr/fr/datasets/r/84a67a3b-08a7-4001-98e6-231c74a98139
-    The id of the dataset is the last part of this URL
-    The name of the dataset is dis-YEAR.zip (but the format could potentially change).
-    :param year: The year from which we want to get the dataset information.
-    :return: A dict with the id and name of the dataset.
-    """
-    edc_dis_files_info_by_year = {
-        "2024": {"id": "84a67a3b-08a7-4001-98e6-231c74a98139", "name": "dis-2024.zip"},
-        "2023": {"id": "c89dec4a-d985-447c-a102-75ba814c398e", "name": "dis-2023.zip"},
-        "2022": {"id": "a97b6074-c4dd-4ef2-8922-b0cf04dbff9a", "name": "dis-2022.zip"},
-        "2021": {"id": "d2b432cc-3761-44d3-8e66-48bc15300bb5", "name": "dis-2021.zip"},
-        "2020": {"id": "a6cb4fea-ef8c-47a5-acb3-14e49ccad801", "name": "dis-2020.zip"},
-        "2019": {"id": "861f2a7d-024c-4bf0-968b-9e3069d9de07", "name": "dis-2019.zip"},
-        "2018": {"id": "0513b3c0-dc18-468d-a969-b3508f079792", "name": "dis-2018.zip"},
-        "2017": {"id": "5785427b-3167-49fa-a581-aef835f0fb04", "name": "dis-2017.zip"},
-        "2016": {"id": "483c84dd-7912-483b-b96f-4fa5e1d8651f", "name": "dis-2016.zip"},
-    }
-    return edc_dis_files_info_by_year[year]
-
-
 def download_extract_insert_yearly_edc_data(year: str):
     """
     Downloads from www.data.gouv.fr the EDC (Eau distribuée par commune) dataset for one year,
@@ -65,30 +42,16 @@ def download_extract_insert_yearly_edc_data(year: str):
         It adds the column "de_partition" based on year as an integer.
     """
 
-    yearly_dataset_info = get_yearly_edc_infos(year=year)
-
     # Dataset specific constants
-    DATA_URL = f"https://www.data.gouv.fr/fr/datasets/r/{yearly_dataset_info['id']}"
-    ZIP_FILE = os.path.join(CACHE_FOLDER, yearly_dataset_info["name"])
+    DATA_URL = (
+        edc_config["source"]["base_url"]
+        + edc_config["source"]["yearly_files_infos"][year]["id"]
+    )
+    ZIP_FILE = os.path.join(
+        CACHE_FOLDER, edc_config["source"]["yearly_files_infos"][year]["zipfile"]
+    )
     EXTRACT_FOLDER = os.path.join(CACHE_FOLDER, f"raw_data_{year}")
-
-    FILES = {
-        "communes": {
-            "filename_prefix": f"DIS_COM_UDI_",
-            "file_extension": ".txt",
-            "table_name": f"edc_communes",
-        },
-        "prelevements": {
-            "filename_prefix": f"DIS_PLV_",
-            "file_extension": ".txt",
-            "table_name": f"edc_prelevements",
-        },
-        "resultats": {
-            "filename_prefix": f"DIS_RESULT_",
-            "file_extension": ".txt",
-            "table_name": f"edc_resultats",
-        },
-    }
+    FILES = edc_config["files"]
 
     logger.info(f"Processing EDC dataset for {year}...")
     response = requests.get(DATA_URL, stream=True)
@@ -106,7 +69,11 @@ def download_extract_insert_yearly_edc_data(year: str):
     for file_info in FILES.values():
         filepath = os.path.join(
             EXTRACT_FOLDER,
-            f"{file_info['filename_prefix']}{year}{file_info['file_extension']}",
+            create_edc_yearly_filename(
+                file_name_prefix=file_info["file_name_prefix"],
+                file_extension=file_info["file_extension"],
+                year=year,
+            ),
         )
 
         if check_table_existence(conn=conn, table_name=f"{file_info['table_name']}"):
@@ -151,17 +118,7 @@ def process_edc_datasets(
     :param custom_years: years to update
     :return:
     """
-    available_years = [
-        "2016",
-        "2017",
-        "2018",
-        "2019",
-        "2020",
-        "2021",
-        "2022",
-        "2023",
-        "2024",
-    ]
+    available_years = edc_config["source"]["available_years"]
 
     if refresh_type == "all":
         years_to_update = available_years
